ab6358c7e391e9475540b9b19efa085333f1ec1b
Matthias Schilling <ma.schill@gmx.de>
2019-04-09 00:53:44 +0200

    create dummy plot with dash

--- src/hello_world.py
+++ src/hello_world.py
@@ -12,24 +12,36 @@ app = dash.Dash(__name__, server=server)
 np.random.seed(101)
 random_x = np.random.randint(1,21,20)
 random_y = np.random.randint(1,21,20)
+consumption_x = np.arange(24)
+consumption_y = np.array([2,1,1,0,0,0,1,4,4,3,3,7,8,5,3,2,1,2,6,6,3,3,2,2])*2
+production_x = np.arange(24)
+production_y = np.array([0,0,0,0,0,0,0,0.9,3.5,7.4,12.3,16.4,18.6,19.2,18.4,15.8,11.7,6.7,2.3,0.2,0,0,0,0])
+assert(len(consumption_x)==len(consumption_y))
+assert(len(production_x)==len(production_y))
 
 app.layout=html.Div(children=[
-    html.H1('Hello World'),
-    
+  
     doc.Graph(
         id='scatter',
         figure={
             'data': [
                 go.Scatter(
-                    x=random_x,
-                    y=random_y,
-                    mode='markers'
-                )
+                    x=consumption_x,
+                    y=consumption_y,
+                    fill='tozeroy',
+                    name='Verbrauch'
+                ),
+                go.Scatter(
+                    x=production_x,
+                    y=production_y,
+                    fill='tonexty',
+                    name='PV Erzeugung'
+                ),
             ],
             'layout': go.Layout(
-                title = 'Here is My Scatter Plot',
-                xaxis = {'title': 'Here is My X-Axis'},
-                yaxis = {'title': 'Here is My Y-Axis'}
+                title = 'Dummy Verbrauch-Erzeugungs-Plot',
+                xaxis = {'title': 'Uhrzeit'},
+                yaxis = {'title': 'kW'}
             )
         }
     )
