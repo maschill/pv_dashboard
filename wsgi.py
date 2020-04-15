348330654e73f454586d367c7af99a6bae6a4caf
Johannes Schilling <dario@deaktualisierung.org>
2020-04-16 00:46:04 +0200

    fix wsgi file

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,12 @@
 import sys
 import os
 
+# add this path to python include path
 sys.path.append(os.path.abspath(os.path.dirname(__file__)))
 sys.path.append(os.path.join(os.path.abspath(os.path.dirname(__file__)), "src"))
 
-from src.hello_world import app
+# activate virtual env
+activate_venv_file = os.path.join(os.path.dirname(__file__), "solarfoo_venv", "bin", "activate_this.py")
+exec(open(activate_venv_file).read(), {'__file__': activate_venv_file})
+
+from src.hello_world import app, server
