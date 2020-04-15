f7a3b4dd39607ba0cd399a9deaafb20b613d4bfe
Johannes Schilling <dario@deaktualisierung.org>
2020-04-16 00:10:39 +0200

    Add --print-only option

--- src/parse_logfile.py
+++ src/parse_logfile.py
@@ -1,5 +1,8 @@
 import numpy as np
 import argparse
+import sys
+
+from tabulate import tabulate
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
@@ -12,7 +15,8 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="parse pv logfiles and write them to db")
     parser.add_argument("filename", metavar="filename", type=str, help="logfile that is parsed")
     parser.add_argument("-d", "--delete-old",dest="delete", action="store_true", help="delete all old records and write everything from the logfile to the db")
-   
+    parser.add_argument("-p", "--print-only", action="store_true", help="print parsed data instead of storing to DB")
+
     args = parser.parse_args()
 
     #fix some values
@@ -21,7 +25,8 @@ if __name__ == '__main__':
     wi, wu = 0,0
     
     dt = np.dtype([
-        ('a', np.int32), # idk
+        ('a', np.int16), # idk
+        ('b', np.int16), # important for daily total/max/?? differentiation
         ('indicator', np.int32), # 6 for hourly
         ('timestamp', np.int32), # unix time in s
         ('value', np.int32) # in kwh*100
@@ -30,6 +35,20 @@ if __name__ == '__main__':
     with open(args.filename, 'rb') as fo:
         fo.seek(8)
         arr = np.fromfile(fo, dtype=dt, count=-1, sep='')
+
+    if args.print_only:
+        table = []
+        for _a, b, ind, ts, val in arr:
+            formatted = datetime.fromtimestamp(ts).isoformat()
+            what_b = {
+                    12290: 'total',
+                    4149: 'peak',
+                    4145: 'insulation',
+                }
+            table.append([formatted, what_b.get(b, b), ind, val])
+
+        print(tabulate(table, ["date/time", "b", "type", "value"], tablefmt="grid"))
+        sys.exit(0)
 
     connection = None
     with open('src/db_config.txt', 'r') as fo:
@@ -46,7 +65,7 @@ if __name__ == '__main__':
     wid = 4
     messdaten = []
     insert_time = datetime.now()
-    for a, ind, ts, val in arr[1:]:
+    for _a, _b, ind, ts, val in arr[1:]:
         if args.delete or ts > last_timestamp:
             t = datetime.fromtimestamp(ts)
             messdaten.append(Messdaten(wid, t, timedelta(0,60,0), 0, gu, gi, gp, wu, wi, int(val), 30, insert_time))
